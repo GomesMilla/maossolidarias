fa9000d410af88a19be5be5e7f019db7ea1e1e9b
GomesMilla <gomesmilla@pop-os.localdomain>
2023-06-08 21:13:41 -0300

    Lib django-on-heroku

--- configuration/settings.py
+++ configuration/settings.py
@@ -146,12 +146,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
