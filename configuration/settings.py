8fb4f01fe21e0b52299fa7fe12aa76c93410e96f
GomesMilla <gomesmilla@pop-os.localdomain>
2023-06-08 21:06:12 -0300

    Lib django-on-heroku

--- configuration/settings.py
+++ configuration/settings.py
@@ -146,10 +146,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, "static")
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
