e9a5354ec07115aed78a0ae6c608e76464f2750f
GomesMilla <gomesmilla@pop-os.localdomain>
2023-08-28 21:40:20 -0300

    :pushpin: Ajustando funcionalidade de home

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from django.views.generic import TemplateView
-from .views import PedirDoacaoCreateView, SolicitacoesListView, SolicitacaoDetailView
+from .views import PedirDoacaoCreateView, SolicitacoesListView, SolicitacaoDetailView, IntroductionView
 from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     path("objetivo/", TemplateView.as_view(template_name="presentation/objetivo.html"), name="objetivo"),
-    path("introducao/", TemplateView.as_view(template_name="presentation/introduction.html"), name="introducao"),
+    path("introducao/", IntroductionView.as_view(), name="introducao"),
     path("pedir-doacao/", login_required(PedirDoacaoCreateView.as_view()), name="pedir_doacao"),
     path("solicitacoes/", SolicitacoesListView.as_view(), name="solicitacoes"),
     path("ver-solicitacao/<int:pk>", SolicitacaoDetailView.as_view(), name="ver_solicitacao"),
