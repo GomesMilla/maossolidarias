6b4d3a612b3dd72d4e9947c24dd49f48f3d0cffb
GomesMilla <gomesmilla@pop-os.localdomain>
2023-09-23 16:22:38 -0300

    :rotating_light: Adicionando login required onde precisa nas urls

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path("pedir-doacao/", login_required(PedirDoacaoCreateView.as_view()), name="pedir_doacao"),
     path("solicitacoes/", SolicitacoesListView.as_view(), name="solicitacoes"),
     path("solicitacoes-por-categoria/<int:pk>", SolicitacoesPorCategoria.as_view(), name="solicitacoes_por_categoria"),
-    path("solicitacao/<int:pk>/edit", PedirDoacaolUpdate.as_view(), name="editar_solicitacao"),
+    path("solicitacao/<int:pk>/edit", login_required(PedirDoacaolUpdate.as_view()), name="editar_solicitacao"),
     path("ver-solicitacao/<int:pk>", SolicitacaoDetailView.as_view(), name="ver_solicitacao"),
-    path("inativar-solicitacao/<int:pk>", DesativarSolicitacao.as_view(), name="inativar_solicitacao"),
-    path("relatorios/<int:pk>", RelatoriosView.as_view(), name="relatorios"),
-    path("contatos/<int:pk>", ContatosSolicitacaoView.as_view(), name="contatos"),
+    path("inativar-solicitacao/<int:pk>", login_required(DesativarSolicitacao.as_view()), name="inativar_solicitacao"),
+    path("relatorios/<int:pk>", login_required(RelatoriosView.as_view()), name="relatorios"),
+    path("contatos/<int:pk>", login_required(ContatosSolicitacaoView.as_view()), name="contatos"),
 
 ]
